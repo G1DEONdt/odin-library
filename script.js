c59eb06e54b3b47deb16f1d3dd83ec51b47eddd0
G1DEONdt <165989663+G1DEONdt@users.noreply.github.com>
2024-05-20 22:51:00 +1000

    Begin refactor

--- script.js
+++ script.js
@@ -12,18 +12,30 @@ const myLibrary = [{
 }];
 
 
-function Book(title, author, pageCount, status) {
+function Book(id, title, author, pageCount, status) {
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pageCount = pageCount;
     this.status = status;
 
-    function removeBook() {
-        
+    this.removeBook = function () {
+        myLibrary.splice(this.id, 1);
+    }
+
+    this.toggleStatus = function () {
+        if (this.status == "read") {
+            this.status = "unread"
+            myLibrary[this.id].classList.remove("read");
+        } else {
+            this.status = "read";
+            myLibrary[this.id].classList.add("read");
+        }
     }
 }
 
 function addBookToLibrary() {
+    const id = myLibrary.length;
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const pageCount = document.getElementById("pageCount").value;
@@ -40,9 +52,9 @@ function redrawGrid() {
             <h2>${myLibrary[x].title}</h2>
             <p>${myLibrary[x].author}</p>
             <p><span>${myLibrary[x].pageCount}</span> pages</p>
-            <p>${myLibrary[x].status}</p>
             <div class="card-buttons">
-                <button onclick="removeBook(${x})" type="button">x</button>
+                <button onclick="${myLibrary[x].removeBook()}" type="button">x</button>
+                <button onClick="${myLibrary[x].toggleStatus()}" type="button">${capitaliseFirstLetter(myLibrary[x].status)}</button>
             </div>
         </div>
         `
@@ -51,12 +63,16 @@ function redrawGrid() {
     
 }
 
-redrawGrid();
+// redrawGrid();
 
-function removeBook(index) {
-    myLibrary.pop(index);
-    redrawGrid();
+function capitaliseFirstLetter(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+// function removeBook(index) {
+//     myLibrary.pop(index);
+//     redrawGrid();
+// }
 
 function resetForm() {
     form.reset();
