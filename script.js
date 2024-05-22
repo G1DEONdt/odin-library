3606a0abea3d1a426b68f54ca835a78475b6eb3e
G1DEONdt <165989663+G1DEONdt@users.noreply.github.com>
2024-05-22 22:53:45 +1000

    Refactor
    Added functions to Book constructor.
    
    Styled status button

--- script.js
+++ script.js
@@ -4,11 +4,8 @@ const form = document.querySelector("#form");
 const button = document.querySelector(".add");
 const submit = document.querySelector(".submit");
 const cancel = document.querySelector(".cancel");
+let gridNodeList = document.querySelectorAll(".toggle");
 const myLibrary = [{
-    title: "Harry Potter",
-    author: "JK Rowling",
-    pageCount: "182",
-    status: "unread"
 }];
 
 
@@ -19,18 +16,22 @@ function Book(id, title, author, pageCount, status) {
     this.pageCount = pageCount;
     this.status = status;
 
+    this.call = function () {
+        console.log("HEY");
+    }
+
     this.removeBook = function () {
         myLibrary.splice(this.id, 1);
+        redrawGrid();
     }
 
     this.toggleStatus = function () {
         if (this.status == "read") {
             this.status = "unread"
-            myLibrary[this.id].classList.remove("read");
         } else {
             this.status = "read";
-            myLibrary[this.id].classList.add("read");
         }
+        redrawGrid();
     }
 }
 
@@ -45,27 +46,22 @@ function addBookToLibrary() {
 
 function redrawGrid() {
     grid.innerHTML = "";
-    for (x = 0; x < myLibrary.length; x++) {
+    for (x = 1; x < myLibrary.length; x++) {
         let currentCard = `
-        <div class="card">
+        <div class="card ${x}">
             <img src="images/Harry_Potter_and_the_Prisoner_of_Azkaban.jpg" alt="cover for harry potter">
             <h2>${myLibrary[x].title}</h2>
             <p>${myLibrary[x].author}</p>
             <p><span>${myLibrary[x].pageCount}</span> pages</p>
             <div class="card-buttons">
-                <button onclick="" type="button">x</button>
-                <button onClick="" type="button">Read</button>
+                <button onclick="myLibrary[${x}].removeBook()" type="button">x</button>
+                <button onclick="myLibrary[${x}].toggleStatus()" type="button" class="toggle ${myLibrary[x].status}">${myLibrary[x].status}</button>
             </div>
         </div>
         `
         grid.innerHTML+= currentCard;
     }
-    
-}
-
-
-function removeCard() {
-    
+    refreshNodeList();
 }
 
 function capitaliseFirstLetter(string){
@@ -78,6 +74,10 @@ function resetForm() {
 
 function closeForm() {
     formContainer.classList.remove("active");
+}
+
+function refreshNodeList() {
+    gridNodeList = document.querySelectorAll(".toggle");
 }
 
 
