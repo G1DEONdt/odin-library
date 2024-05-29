91c4d02579a55dc4f7336baf6aaea5885e6addd0
G1DEONdt <165989663+G1DEONdt@users.noreply.github.com>
2024-05-29 11:12:16 +1000

    Refactored using Classes

--- script.js
+++ script.js
@@ -4,69 +4,70 @@ const form = document.querySelector("#form");
 const button = document.querySelector(".add");
 const submit = document.querySelector(".submit");
 const cancel = document.querySelector(".cancel");
-let gridNodeList = document.querySelectorAll(".toggle");
-const myLibrary = [{
-}];
 
-
-function Book(id, title, author, pageCount, status) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.pageCount = pageCount;
-    this.status = status;
-
-    this.call = function () {
-        console.log("HEY");
+class Library {
+    constructor () {
+        this.bookcase = [];
+        this.gridNodeList = document.querySelectorAll(".toggle");
+    } 
+    addBook() {
+        const id = this.bookcase.length;
+        const title = document.getElementById("title").value;
+        const author = document.getElementById("author").value;
+        const pageCount = document.getElementById("pageCount").value;
+        const status = document.getElementById("status").value;
+        this.bookcase.push(new Book(id, title, author, pageCount, status));
     }
-
-    this.removeBook = function () {
-        myLibrary.splice(this.id, 1);
-        redrawGrid();
+    removeBook(id) {
+        this.bookcase.splice(id, 1);
+        this.render();
     }
-
-    this.toggleStatus = function () {
-        if (this.status == "read") {
-            this.status = "unread"
+    toggleStatus(id) {
+        if (this.bookcase[id].status == "read") {
+            this.bookcase[id].status = "unread"
         } else {
-            this.status = "read";
+            this.bookcase[id].status = "read";
         }
-        redrawGrid();
+        this.render();
     }
-}
-
-function addBookToLibrary() {
-    const id = myLibrary.length;
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-    const pageCount = document.getElementById("pageCount").value;
-    const status = document.getElementById("status").value;
-    myLibrary.push(new Book(id, title, author, pageCount, status));
-}
-
-function redrawGrid() {
-    grid.innerHTML = "";
-    for (x = 1; x < myLibrary.length; x++) {
-        let currentCard = `
-        <div class="card ${x}">
-            <img src="images/Harry_Potter_and_the_Prisoner_of_Azkaban.jpg" alt="cover for harry potter">
-            <h2>${myLibrary[x].title}</h2>
-            <p>${myLibrary[x].author}</p>
-            <p><span>${myLibrary[x].pageCount}</span> pages</p>
-            <div class="card-buttons">
-                <button onclick="myLibrary[${x}].removeBook()" type="button">Delete</button>
-                <button onclick="myLibrary[${x}].toggleStatus()" type="button" class="toggle ${myLibrary[x].status}">${capitaliseFirstLetter(myLibrary[x].status)}</button>
+    capitaliseFirstLetter(string){
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    refreshNodeList() {
+        this.gridNodeList = document.querySelectorAll(".toggle");
+    }
+    render() {
+        grid.innerHTML = "";
+        for (let x = 0; x < library.bookcase.length; x++) {
+            let currentCard = `
+            <div class="card ${x}">
+                <img src="images/Harry_Potter_and_the_Prisoner_of_Azkaban.jpg" alt="cover for harry potter">
+                <h2>${library.bookcase[x].title}</h2>
+                <p>${library.bookcase[x].author}</p>
+                <p><span>${library.bookcase[x].pageCount}</span> pages</p>
+                <div class="card-buttons">
+                    <button onclick="library.removeBook(${x})" type="button">Delete</button>
+                    <button onclick="library.toggleStatus(${x})" type="button" class="toggle ${library.bookcase[x].status}">${this.capitaliseFirstLetter(library.bookcase[x].status)}</button>
+                </div>
             </div>
-        </div>
-        `
-        grid.innerHTML+= currentCard;
+            `
+            grid.innerHTML+= currentCard;
+        }
+        this.refreshNodeList();
     }
-    refreshNodeList();
 }
 
-function capitaliseFirstLetter(string){
-    return string.charAt(0).toUpperCase() + string.slice(1);
+class Book {
+    constructor (id, title, author, pageCount, status) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pageCount = pageCount;
+        this.status = status;
+    }
 }
+
+const library = new Library();
 
 function resetForm() {
     form.reset();
@@ -76,18 +77,13 @@ function closeForm() {
     formContainer.classList.remove("active");
 }
 
-function refreshNodeList() {
-    gridNodeList = document.querySelectorAll(".toggle");
-}
-
-
 button.addEventListener("click", () => {
     formContainer.classList.add("active");
 });
 
 submit.addEventListener("click", () => {
-    addBookToLibrary();
-    redrawGrid();
+    library.addBook();
+    library.render();
     resetForm();
     closeForm();
 })
